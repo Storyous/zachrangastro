47e4337305f65bf3df543d17ecf2e3d8d90ea4c4
Veronika Šilhová <eaavsi@students.eaaa.dk>
2020-03-19 18:04:20 +0100

    add Insta images

--- src/components/InstagramFeed.vue
+++ src/components/InstagramFeed.vue
@@ -6,21 +6,58 @@
           >gastro</span
         >
       </h2>
-      <div v-for="post of posts" :key="post.id"></div>
+      <div class="row">
+        <div
+          v-for="post of posts"
+          :key="post.id"
+          class="d-block col-sm-6 col-md-4 col-lg-4 image-wrapper pt-4"
+        >
+          <img :src="require(`@/assets/img/${post.image}`)" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          image: "gastro_1.jpg"
+        },
+        {
+          id: 2,
+          image: "gastro_2.jpg"
+        },
+        {
+          id: 3,
+          image: "gastro_3.jpg"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped lang="scss">
 .instagram-feed {
   margin: 1rem;
   border: 1px solid #d45354;
-  padding: 1rem;
+  padding: 1.5rem;
   border-radius: 10px;
+
+  h2 {
+    margin-bottom: 0;
+  }
+
+  .image-wrapper {
+    img {
+      width: 100%;
+    }
+  }
 }
 
 h2 {
