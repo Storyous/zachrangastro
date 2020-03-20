241c85c799f1f6849505066eedfbfe91f32f58a4
Veronika Šilhová <eaavsi@students.eaaa.dk>
2020-03-20 13:14:23 +0100

    fix Google form links

--- src/components/GoogleForm.vue
+++ src/components/GoogleForm.vue
@@ -17,15 +17,15 @@ export default {
     source: function() {
       switch (this.type) {
         case "takeaway":
-          return "https://docs.google.com/forms/d/e/1FAIpQLSd1zq8vseXmaiABAEZAE826nYJJum9-eE96F6cf0D4ldiRQLw/viewform";
+          return "https://docs.google.com/forms/d/1S-YJ2_0GggbSViXZlRf5uF7VV0wgF0ddsJ9O2nnur_k/edit?ts=5e74a8c4";
         case "delivery":
           return "https://docs.google.com/forms/d/1b45vWFV_3mPlW07nFoaAH_GQSfzma8u5-ZO_AnDc13c/edit";
         case "supplier":
           return "https://docs.google.com/forms/d/19ezYuPkXIktpI5komA80ddhWtsS9FC6UgAlA3p_3ld0/edit";
         case "personnel":
-          return "https://docs.google.com/forms/d/12qXlyPsQh4GkvQ98YWWwKwscXehCWZ_CQcfDZf3a5rw/edit";
+          return "https://docs.google.com/forms/d/1teuVHxwqqeVuYgOP7IjSbKzzjfLjuKn4ax1ov8PT1Cs/edit";
         case "waiter":
-          return "https://docs.google.com/forms/d/1b45vWFV_3mPlW07nFoaAH_GQSfzma8u5-ZO_AnDc13c/edit";
+          return "https://docs.google.com/forms/d/12qXlyPsQh4GkvQ98YWWwKwscXehCWZ_CQcfDZf3a5rw/edit";
         default:
           return null;
       }
