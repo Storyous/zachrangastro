132b7dec9babe339cb6be6be980deccd0590d570
Stanislav Gurnik <sgurnik@gmail.com>
2020-03-23 11:33:46 +0100

    Refactor & preload iframes

--- src/components/GoogleForm.vue
+++ src/components/GoogleForm.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { links } from "../googleForms.js";
+
 export default {
   name: "GoogleForm",
   props: {
@@ -18,20 +20,10 @@ export default {
   },
   computed: {
     source: function() {
-      switch (this.type) {
-        case "takeaway":
-          return "https://docs.google.com/forms/d/1S-YJ2_0GggbSViXZlRf5uF7VV0wgF0ddsJ9O2nnur_k/edit?ts=5e74a8c4";
-        case "delivery":
-          return "https://docs.google.com/forms/d/1b45vWFV_3mPlW07nFoaAH_GQSfzma8u5-ZO_AnDc13c/edit";
-        case "supplier":
-          return "https://docs.google.com/forms/d/19ezYuPkXIktpI5komA80ddhWtsS9FC6UgAlA3p_3ld0/edit";
-        case "personnel":
-          return "https://docs.google.com/forms/d/1teuVHxwqqeVuYgOP7IjSbKzzjfLjuKn4ax1ov8PT1Cs/edit";
-        case "waiter":
-          return "https://docs.google.com/forms/d/12qXlyPsQh4GkvQ98YWWwKwscXehCWZ_CQcfDZf3a5rw/edit";
-        default:
-          return null;
+      if (this.type in links) {
+        return links[this.type];
       }
+      return null;
     },
     styles: () => {
       return {
